1327bbd6d59184309772c6b33f55f2b463d4d3bc
nukeguys <nukeguys0@gmail.com>
2024-12-29 15:46:39 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const unused = 'unused';
   const [count, setCount] = useState(0);
 
   return (
